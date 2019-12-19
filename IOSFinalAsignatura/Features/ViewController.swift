eab9caee882b06ac5ee6f537938e01d88d2e72ba
Manu <espesito16@gmail.com>
2019-12-19 08:58:29 -0800

    Folder structure and firebase configuration Some pods foor use firebase install, Firebase configure implements in the appDelegate and folder structure for the proyect created.

---
+++ IOSFinalAsignatura/Features/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  IOSFinalAsignatura
+//
+//  Created by Manuel Espeso Martin on 11/12/19.
+//  Copyright © 2019 Manuel Espeso Martin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'IOSFinalAsignatura' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for IOSFinalAsignatura
+ 	pod 'Firebase'
+  	pod 'FirebaseAuth'
+  	pod 'FirebaseFirestore'
+  	pod 'FirebaseCore'
+
+end
