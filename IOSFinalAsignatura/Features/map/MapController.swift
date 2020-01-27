8f52184c0e4ba7021f15ae0afb9ecd80d0896cab
alvaro <alvaro@alvaros-Mac.local>
2020-01-27 20:03:24 +0100

    added icon tab bar and view controller

---
+++ IOSFinalAsignatura/Features/map/MapController.swift
@@ -0,0 +1,23 @@
+//
+//  MapController.swift
+//  IOSFinalAsignatura
+//
+//  Created by alvaro on 27/01/2020.
+//  Copyright © 2020 Manuel Espeso Martin. All rights reserved.
+//
+
+import UIKit
+import AMTabView
+
+class MapController: UIViewController, TabItem {
+    var tabImage: UIImage? {
+           return UIImage(named: "mapIcon")
+       }
+       
+       override func viewDidLoad() {
+           super.viewDidLoad()
+           
+       }
+    
+    
+}
